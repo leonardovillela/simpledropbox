8446c1ec0933f9e5b8131a2b8757b8a29ba045e7
Leonardo Villela <leonardo.villela@concrete.com.br>
2018-04-25 12:18:10 -0300

    Adding AWS hosted ui.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Route, Switch } from 'react-router';
 import { withOAuth } from 'aws-amplify-react';
 
@@ -9,11 +9,16 @@ import Menu from './Menu';
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/home" component={Home} />
-        <Route path="/logout" component={Logout} />
-        <Route path="/" component={Menu} />
-      </Switch>
+      <Fragment>
+        <button onClick={this.props.OAuthSignIn}>
+          Sign in with AWS
+        </button>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" component={Menu} />
+        </Switch>
+      </Fragment>
     );
   }
 }
